0e1b080a94aa8962263bbc1aa85dc04c5e4cee57
skyguide33 <761746813@qq.com>
2026-06-05 10:28:19 +0800

    修正了LQB_CB_N_G.cpp

--- Pbm/lanqiao/16th_C_C++_B_National/G.cpp
+++ Pbm/lanqiao/16th_C_C++_B_National/G.cpp
@@ -8,16 +8,22 @@ ll a[200005];
 
 int main() {
   cin.tie(0) -> sync_with_stdio(0);
-	int ans = 0, n; cin >> n >> a[1];
+	ll ans = 0, n; cin >> n >> a[1];
+	int b = 0;
 	for(int i = 2; i <= n; ++i) {
 		cin >> a[i];
 		while(a[i] < a[i-1]) {
 			a[i] <<= 1;
 			++ans;
 		}
-		// cout << a[i] << ' ';
+		ans += b;
+		while(a[i] >= 1e10) {
+			a[i] >>= 1;
+			++b;
+		}
+		cout << a[i] << ' ';
 	}
-	// cout << endl;
+	cout << endl;
 	cout << ans << endl;
   return 0;
 }
